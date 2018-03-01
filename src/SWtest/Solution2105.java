f599b86692322f917d56a916ae57292556043df4
Do Gyeong Seo <dgseo8981@gmail.com>
2018-03-02 00:47:22 +0900

    20180301

--- src/SWtest/Solution2105.java
+++ src/SWtest/Solution2105.java
@@ -1,10 +1,25 @@
 package SWtest;
 
-public class Solution2105 {
+import java.util.Scanner;
+
+public class Solution2105 {	//디저트 카페
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int t=0; t<T; t++) {
+			int N=sc.nextInt();
+			int[][] map=new int[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					map[i][j]=sc.nextInt();
+				}
+			}
+			
+		}
 		
+		sc.close();
 	}
 
 }
