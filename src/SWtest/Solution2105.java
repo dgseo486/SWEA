2317cc331c9072e41dfc96ed9de277d16b2010f2
Do Gyeong Seo <dgseo8981@gmail.com>
2018-04-02 19:32:03 +0900

    20180402

--- src/SWtest/Solution2105.java
+++ src/SWtest/Solution2105.java
@@ -4,60 +4,58 @@ import java.util.Scanner;
 
 public class Solution2105 { // 디저트카페
 
-    static int[][] map = new int[20][20];
-    static boolean[] check = new boolean[101];
+    static int[][] map;
+    static boolean[][] visited;
+    static boolean[] check;
     static int startI = 0;
     static int startJ = 0;
     static int N = 0;
     static int maxDessert;
     static int[][] dir = { { 1, 1 }, { 1, -1 }, { -1, -1 }, { -1, 1 } };
 
-    static void init() {
-        for (int i = 0; i < 101; i++) {
-            check[i] = false;
-        }
-    }
-
     static void dfs(int i, int j, int depth, int direction) {
-        if (startI == i && startJ == j && direction > 2) {
+
+        int nextI = i + dir[direction][0];
+        int nextJ = j + dir[direction][1];
+
+        if (nextI < 0 || nextI >= N || nextJ < 0 || nextJ >= N) {
+            return;
+        }
+
+        if (startI == nextI && startJ == nextJ && direction > 2) {
             if (maxDessert < depth) {
                 maxDessert = depth;
             }
             return;
         }
 
-        check[map[i][j]] = true;
-
-        int nextI = i + dir[direction][0];
-        int nextJ = j + dir[direction][1];
-        if (nextI < 0 || nextI >= N || nextJ < 0 || nextJ >= N) {
+        if (check[map[nextI][nextJ]]) {
             return;
         }
         
+        if(visited[nextI][nextJ]) {
+            return;
+        }
+
+        check[map[nextI][nextJ]] = true;
+        visited[nextI][nextJ] = true;
         
-        if (startI == nextI && startJ == nextJ && direction > 2) {
-            dfs(nextI, nextJ, depth + 1, 3);
-            check[map[nextI][nextJ]] = false;
-        } else if (direction == 0 && check[map[nextI][nextJ]] == false) {
+        if (direction == 0) {
             dfs(nextI, nextJ, depth + 1, 0);
-            check[map[nextI][nextJ]] = false;
             dfs(nextI, nextJ, depth + 1, 1);
-            check[map[nextI][nextJ]] = false;
-        } else if (direction == 1 && check[map[nextI][nextJ]] == false) {
+        } else if (direction == 1) {
             dfs(nextI, nextJ, depth + 1, 1);
-            check[map[nextI][nextJ]] = false;
             dfs(nextI, nextJ, depth + 1, 2);
-            check[map[nextI][nextJ]] = false;
-        } else if (direction == 2 && check[map[nextI][nextJ]] == false) {
+        } else if (direction == 2) {
             dfs(nextI, nextJ, depth + 1, 2);
-            check[map[nextI][nextJ]] = false;
             dfs(nextI, nextJ, depth + 1, 3);
-            check[map[nextI][nextJ]] = false;
-        } else if (direction == 3 && check[map[nextI][nextJ]] == false) {
+        } else if (direction == 3) {
             dfs(nextI, nextJ, depth + 1, 3);
-            check[map[nextI][nextJ]] = false;
         }
         
+        check[map[nextI][nextJ]] = false;
+        visited[nextI][nextJ] = false;
+
     }
 
     public static void main(String[] args) {
@@ -65,6 +63,9 @@ public class Solution2105 { // 디저트카페
         int T = sc.nextInt();
         for (int t = 0; t < T; t++) {
             N = sc.nextInt();
+            map = new int[N][N];
+            visited = new boolean[N][N];
+            check = new boolean[101];
             maxDessert = -1;
 
             for (int i = 0; i < N; i++) {
@@ -75,13 +76,15 @@ public class Solution2105 { // 디저트카페
 
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
+                    check[map[i][j]] = true;
+                    visited[i][j] = true;
                     startI = i;
                     startJ = j;
-                    dfs(i, j, 0, 0);
-                    init();
+                    dfs(i, j, 1, 0);
+                    check[map[i][j]] = false;
+                    visited[i][j] = false;
                 }
             }
-            
             System.out.println("#" + (t + 1) + " " + maxDessert);
         }
         sc.close();
