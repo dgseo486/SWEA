972cbaffcc896f81a418b9a4439ab8fb9ec51b23
Do Gyeong Seo <dgseo8981@gmail.com>
2018-04-04 13:35:45 +0900

    20180404

--- src/D2/Solution1926.java
+++ src/D2/Solution1926.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class Solution1926 {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int cnt=0;
-        
-        for(int i=1; i<=n; i++) {
-        	int temp=i;
-        	while(temp!=0) {
-        		if(temp%10==3 || temp%10==6 || temp%10==9) {
-        			cnt++;
-        		}
-        		temp=temp/10;
-        	}
-        	
-        	if(cnt==0) {
-        		System.out.print(i+" ");
-        		cnt=0;
-        	}else {
-        		for(int j=1; j<=cnt; j++) {
-        			System.out.print("-");
-        		}
-        		System.out.print(" ");
-        		cnt=0;
-        	}
-        	
+    public static void main(String[] args) { // 간단한 369게임
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int temp = i;
+            while (temp != 0) {
+                if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9) {
+                    cnt++;
+                }
+                temp = temp / 10;
+            }
+
+            if (cnt == 0) {
+                System.out.print(i + " ");
+                cnt = 0;
+            } else {
+                for (int j = 1; j <= cnt; j++) {
+                    System.out.print("-");
+                }
+                System.out.print(" ");
+                cnt = 0;
+            }
+
         }
         sc.close();
-	}
+    }
 }
---
+++ src/SWtest/Solution2382.java
@@ -0,0 +1,9 @@
+package SWtest;
+
+public class Solution2382 {
+
+    public static void main(String[] args) { // 미생물 격리
+        
+    }
+
+}
